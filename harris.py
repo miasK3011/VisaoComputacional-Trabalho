910b06c06a20534b7a604d730803bfb2613969db
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-01-22 18:52:42 -0300

    Alterações de parametros do harris

--- harris.py
+++ harris.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2 as cv
-filename = 'imgs/dsc02596.jpg'
+filename = 'imgs/exemplo.png'
 img = cv.imread(filename)
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 gray = np.float32(gray)
-dst = cv.cornerHarris(gray,2,3,0.04)
+dst = cv.cornerHarris(gray,5,3,0.01)
 print("Quantidade de pontos: ", len(dst))
 #result is dilated for marking the corners, not important
 dst = cv.dilate(dst,None)
