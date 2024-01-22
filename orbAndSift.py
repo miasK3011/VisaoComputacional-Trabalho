2e2480268e30d0a5016ccd8a157f774a362e2960
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-01-22 18:53:01 -0300

    Alterações de parametros

--- orbAndSift.py
+++ orbAndSift.py
@@ -3,12 +3,16 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img1 = cv.imread('imgs/dsc02595.jpg', cv.IMREAD_GRAYSCALE)  # queryImage
-img2 = cv.imread('imgs/dsc02596.jpg', cv.IMREAD_GRAYSCALE)  # trainImage
+img1 = cv.imread('imgs/exemplo.png', cv.IMREAD_GRAYSCALE)  # queryImage
+img2 = cv.imread('imgs/exemplo2.png', cv.IMREAD_GRAYSCALE)  # trainImage
+
+nFeatures = 20
+edgeTreshold = 1
+sigma = 0.1
 
 # Initiate SIFT detector
-sift = cv.SIFT_create()
-orb = cv.ORB_create()
+sift = cv.SIFT.create(nfeatures=nFeatures, edgeThreshold=edgeTreshold, sigma=sigma)
+orb = cv.ORB.create(nfeatures=nFeatures, edgeThreshold=edgeTreshold, scoreType=1)
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = orb.detectAndCompute(img1, None)
@@ -20,10 +24,10 @@ _, des2_sift = sift.compute(img2, kp2)
 
 # BFMatcher with default params
 bf = cv.BFMatcher()
-matchesSift = bf.knnMatch(des1_sift, des2_sift, k=2)
-matchesOrb = bf.knnMatch(des1, des2, k=2)
+matchesSift = bf.knnMatch(des1_sift, des2_sift, k=3)
+matchesOrb = bf.knnMatch(des1, des2, k=3)
 
-# Apply ratio test
+""" # Apply ratio test
 goodOrb = []
 for m, n in matchesOrb:
     if m.distance < 0.60 * n.distance:
@@ -35,15 +39,15 @@ for m, n in matchesSift:
         goodSift.append([m])
 
 accuracyOrb = (len(goodOrb)/len(matchesOrb))*100
-accuracySIFT = (len(goodSift)/len(matchesSift))*100
+accuracySIFT = (len(goodSift)/len(matchesSift))*100 """
 
 print("Matches Sift: ", len(matchesSift),"\nMatches Orb: ", len(matchesOrb))
-print("Good Sift: ", len(goodSift),"\nGood Orb: ", len(goodOrb))
-print("Acerto ORB: {:.2f} %".format(accuracyOrb), "\nAcerto Sift: {:.2f} %".format(accuracySIFT))
+""" print("Good Sift: ", len(goodSift),"\nGood Orb: ", len(goodOrb))
+print("Acerto ORB: {:.2f} %".format(accuracyOrb), "\nAcerto Sift: {:.2f} %".format(accuracySIFT)) """
 
-img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, goodOrb, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, matchesOrb, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-img4 = cv.drawMatchesKnn(img1, kp1, img2, kp2, goodSift, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img4 = cv.drawMatchesKnn(img1, kp1, img2, kp2, matchesSift, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 plt.subplot(1, 2, 1)
 plt.imshow(img3)
@@ -54,7 +58,6 @@ plt.imshow(img4)
 plt.title('Correspondências detector ORB e descritor SIFT')
 
 # Ajuste o layout para evitar sobreposição
-plt.tight_layout()
 
 # Exiba a janela com os dois subplots
 plt.show()
